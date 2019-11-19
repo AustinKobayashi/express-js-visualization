2fa71c66ee3284a6b2c73a1ecd307d3b471c5a65
Man Kit (Nicholas) Kong <nicholaskong@live.ca>
2019-11-19 00:23:29 -0800

    setup regex for route discovery

--- router_finder.py
+++ router_finder.py
@@ -2,9 +2,9 @@ import os
 import re
 
 symbol_table = dict([('app', 'express()'), ('router', 'express.Router()')])
-regex = {
-    'express_app_var': re.compile(r'app\.use\((?:\'|\")(.+)(?:\'|\"),\s?(\w+)\)')
-}
+regex = {}
+app_var = ''
+router_var = ''
 
 def get_js_files(directory):
     file_paths = []
@@ -14,14 +14,23 @@ def get_js_files(directory):
     return file_paths
 
 def get_routes(file_paths):
+    build_regex()
     for path in file_paths:
         file = open(path)
         for line in file:
-            # app .use()
-            express_app_var = list(symbol_table.keys())[list(symbol_table.values()).index('express()')]
-            # router .get() .post() .put() .delete() .patch()
-            express_router_var = list(symbol_table.keys())[list(symbol_table.values()).index('express.Router()')]
-            
-            if line.strip().startswith((f'{express_app_var}.use')):
+            if line.strip().startswith((f'{app_var}.use')):
                 print(line)
                 
+def build_regex():
+    # app .use()
+    app_var = list(symbol_table.keys())[list(symbol_table.values()).index('express()')]
+    # router .get() .post() .put() .delete() .patch()
+    router_var = list(symbol_table.keys())[list(symbol_table.values()).index('express.Router()')]
+
+    regex['app_use'] = re.compile(r'' + re.escape(app_var) + r'\.use\((?:\'|\")(.+)(?:\'|\"),\s?(\w+)\)')
+    regex['router_get'] = re.compile(r'' + re.escape(router_var) + r'\.get\((?:\'|\")(.+)(?:\'|\"),(.?)')
+    regex['router_put'] = re.compile(r'' + re.escape(router_var) + r'\.put\((?:\'|\")(.+)(?:\'|\"),(.?)')
+    regex['router_post'] = re.compile(r'' + re.escape(router_var) + r'\.post\((?:\'|\")(.+)(?:\'|\"),(.?)')
+    regex['router_delete'] = re.compile(r'' + re.escape(router_var) + r'\.delete\((?:\'|\")(.+)(?:\'|\"),(.?)')
+    regex['router_patch'] = re.compile(r'' + re.escape(router_var) + r'\.patch\((?:\'|\")(.+)(?:\'|\"),(.?)')
+    
