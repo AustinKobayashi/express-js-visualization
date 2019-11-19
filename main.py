d19196f977585b70c7b421294f7ea00627835aa6
Asiapenguin <nicholaskong@live.ca>
2019-11-18 23:34:27 -0800

    started

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import router_finder
 
 def main():
     directory = input("Please enter the path to the directory to scan:\n").strip()
-    router_finder.get_js_files(directory)
+    file_paths = router_finder.get_js_files(directory)
+    router_finder.get_routes(file_paths)
 
 
 if __name__ == "__main__":
--- router_finder.py
+++ router_finder.py
@@ -1,7 +1,27 @@
 import os
+import re
 
+symbol_table = dict([('app', 'express()'), ('router', 'express.Router()')])
+regex = {
+    'express_app_var': re.compile(r'app\.use\((?:\'|\")(.+)(?:\'|\"),\s?(\w+)\)')
+}
 
 def get_js_files(directory):
+    file_paths = []
     for file in os.listdir(directory):
         if file.endswith(".js"):
-            print("Found: " + os.path.join(directory, file))
+            file_paths.append(os.path.join(directory, file))
+    return file_paths
+
+def get_routes(file_paths):
+    for path in file_paths:
+        file = open(path)
+        for line in file:
+            # app .use()
+            express_app_var = list(symbol_table.keys())[list(symbol_table.values()).index('express()')]
+            # router .get() .post() .put() .delete() .patch()
+            express_router_var = list(symbol_table.keys())[list(symbol_table.values()).index('express.Router()')]
+            
+            if line.strip().startswith((f'{express_app_var}.use')):
+                print(line)
+                
